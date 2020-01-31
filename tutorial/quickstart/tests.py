0ef93c54879ae9bddb20fd4032751e7a7f7c8227
Sean McCann <sean.mccann@fullstack.co.za>
2020-01-31 15:05:18 +0200

    Added basic testing

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProjectsTestCase(APITestCase):
+
+    def simple_projects_test(self):
+        response = self.client.get('/api/projects/')
+        self.assertEqual(response.status_code, 200)
